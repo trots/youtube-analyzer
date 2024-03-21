22d6be5b535580f709e26d2fcb8e24a7efac72db
trots <trotsenko93@mail.ru>
2024-03-21 23:51:51 +0300

    Fix sorting by publication date in grep mode

--- test_engines.py
+++ test_engines.py
@@ -28,5 +28,36 @@ class TestStringMethods(unittest.TestCase):
         self.assertIsNone(YoutubeGrepEngine.duration_to_timedelta("0"))
         self.assertIsNone(YoutubeGrepEngine.duration_to_timedelta("0:0:0:0"))
 
+    def test_youtube_grep_publish_time_converter(self):
+        self.assertEqual(timedelta_to_str(YoutubeGrepEngine.published_time_to_timedelta("54 seconds ago")), "00:00:54")
+        self.assertEqual(timedelta_to_str(YoutubeGrepEngine.published_time_to_timedelta("25 minutes ago")), "00:25:00")
+        self.assertEqual(timedelta_to_str(YoutubeGrepEngine.published_time_to_timedelta("16 hours ago")), "16:00:00")
+        self.assertEqual(timedelta_to_str(YoutubeGrepEngine.published_time_to_timedelta("3 days ago")), "3:00:00:00")
+        self.assertEqual(timedelta_to_str(YoutubeGrepEngine.published_time_to_timedelta("2 weeks ago")), "14:00:00:00")
+        self.assertEqual(timedelta_to_str(YoutubeGrepEngine.published_time_to_timedelta("4 months ago")), "120:00:00:00")
+        self.assertEqual(timedelta_to_str(YoutubeGrepEngine.published_time_to_timedelta("3 years ago")), "1080:00:00:00")
+        self.assertEqual(timedelta_to_str(YoutubeGrepEngine.published_time_to_timedelta("Streamed 3 years ago")), "1080:00:00:00")
+
+        self.assertIsNone(YoutubeGrepEngine.published_time_to_timedelta(""))
+        self.assertIsNone(YoutubeGrepEngine.published_time_to_timedelta(None))
+        self.assertIsNone(YoutubeGrepEngine.published_time_to_timedelta("0"))
+        self.assertIsNone(YoutubeGrepEngine.published_time_to_timedelta("1 part ago"))
+
+    def test_youtube_grep_publish_time_sort_cast(self):
+        self.assertEqual(YoutubeGrepEngine.published_time_sort_cast("54 seconds ago"), 54)
+        self.assertEqual(YoutubeGrepEngine.published_time_sort_cast("25 minutes ago"), 2500)
+        self.assertEqual(YoutubeGrepEngine.published_time_sort_cast("16 hours ago"), 160000)
+        self.assertEqual(YoutubeGrepEngine.published_time_sort_cast("3 days ago"), 3000000)
+        self.assertEqual(YoutubeGrepEngine.published_time_sort_cast("2 weeks ago"), 14000000)
+        self.assertEqual(YoutubeGrepEngine.published_time_sort_cast("4 months ago"), 120000000)
+        self.assertEqual(YoutubeGrepEngine.published_time_sort_cast("3 years ago"), 1080000000)
+        self.assertEqual(YoutubeGrepEngine.published_time_sort_cast("9 years ago"), 3240000000)
+        self.assertEqual(YoutubeGrepEngine.published_time_sort_cast("Streamed 3 years ago"), 1080000000)
+
+        self.assertIsNone(YoutubeGrepEngine.published_time_sort_cast(""))
+        self.assertIsNone(YoutubeGrepEngine.published_time_sort_cast(None))
+        self.assertIsNone(YoutubeGrepEngine.published_time_sort_cast("0"))
+        self.assertIsNone(YoutubeGrepEngine.published_time_sort_cast("1 part ago"))
+
 if __name__ == "__main__":
     unittest.main()
