b7d68f6588062a03d911da5d68233f3f0934e114
trots <trotsenko93@mail.ru>
2025-07-19 14:48:40 +0300

    Fix problems with side panel QTabBar
    - Fix unexpected hiding of a first tab of side panel when a current
    tab is not a first tab
    
    - Fix saving of current tab of side panel

--- youtubeanalyzer/widgets.py
+++ youtubeanalyzer/widgets.py
@@ -1,5 +1,7 @@
+from typing import Optional
 from PySide6.QtCore import (
     QSize,
+    QObject,
     Qt,
     QTimer,
     QStringListModel
@@ -15,7 +17,8 @@ from PySide6.QtWidgets import (
     QLineEdit,
     QCompleter,
     QSizePolicy,
-    QMessageBox
+    QMessageBox,
+    QTabWidget
 )
 from youtubeanalyzer.engine import (
     SearchAutocompleteDownloader
@@ -106,3 +109,43 @@ class SearchLineEdit(QLineEdit):
 
     def _on_autocomplete_downloaded(self, autocomplete_list):
         self._autocomplete_model.setStringList(autocomplete_list)
+
+
+class FixedTabWidget(QTabWidget):
+    """
+    A QTabWidget subclass that keeps current tab index across hide/show operations.
+
+    This workaround addresses a Qt framework limitation where QTabWidget resets
+    currentIndex() to 0 when hidden.
+    """
+    def __init__(self, parent: QObject = None):
+        super().__init__(parent)
+        self._last_visible_index: Optional[int] = None
+
+    def get_last_visible_index(self):
+        return self._last_visible_index
+
+    def resizeEvent(self, event: QResizeEvent):
+        super().resizeEvent(event)
+
+        if event.size().width() == 0:
+            self._hide_handler()
+        else:
+            self._show_handler()
+
+    def hideEvent(self, event):
+        self._hide_handler()
+        super().hideEvent(event)
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        self._show_handler()
+
+    def _hide_handler(self):
+        self._last_visible_index = self.currentIndex()
+        self.setCurrentIndex(0)
+
+    def _show_handler(self):
+        if self.count() > 0 and self._last_visible_index is not None:
+            self.setCurrentIndex(self._last_visible_index)
+            self._last_visible_index = None
